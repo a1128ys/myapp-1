5663f0835e52f5c6111c724e539ea95ea01b1338
you1025 <you1025.develop@gmail.com>
2011-12-22 20:28:00 +0900

    hogehoge

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :application, "myapp"
 set :deploy_to, "/Users/rails/myapp"
 
 #set :repository,  "git@github.com:a1128ys/myapp"
-set :repository, "origin"
+set :repository, :origin
 
 default_run_options[:pty] = true
 set :scm, :git
